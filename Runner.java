c05c92d45244f37f5e13796aa8ff148b1f378852
Cansen20 <csen@newroads.org>
2017-09-14 09:33:02 -0700

    did splash stuff

---
+++ Runner.java
@@ -0,0 +1,11 @@
+/* this class runs the start of the program
+*/
+
+public class Runner {
+	//tesing with main
+
+public static void main(String[] args){
+
+//call splash screen	
+splashScreen();
+}
---
+++ splash.java
@@ -0,0 +1,24 @@
+/*
+
+*/
+public class Splash{
+
+		public Splash(){
+			
+			//zero argument constructor - empty for now
+			
+		}
+		
+
+	//this function prints out a custom splash screen
+	public void splashScreen(){
+		System.out.println(" \n" 
+		+ "	____ ____ ____ _ _ _____ \n"
+		+ "	/ ___\\/ _ \\/ __\\/ \\ /|/ \\/ __/	\n"
+		+ "	| \\| / \\|| \\/|| |_||| || \\ \n"
+		+ "	\\___ || \\_/|| __/| | ||| || /_ \n "
+		+ "	\\____/\\____/\\_/ \\_/ \\|\\_/\\____\\ ");
+	}
+
+
+}
