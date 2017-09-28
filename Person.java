b7b9fde30e276fda5a18c5f4231ca1e238f81585
Wyeth Summerford-Ng <wsummerford@newroads.org>
2017-09-28 08:51:21 -0700

    Person class (not done)

---
+++ Person.java
@@ -0,0 +1,16 @@
+
+//sophie september 28 2017
+public class Person{
+	
+	// charatceristics of person
+	
+	private String name;
+	private String gender
+	private String job;
+	private int age;
+	private String Favorite.Color ;
+
+	
+	//hjh
+	
+		public Person() 
