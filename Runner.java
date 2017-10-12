825edf6682ee0eedb97d2cbcd861ed40fb00484a
benjyrr <31515007+benjyrr@users.noreply.github.com>
2017-10-12 09:47:35 -0700

    this week's project (almost done??)

--- Runner.java
+++ Runner.java
@@ -13,56 +13,35 @@ public class Runner {
 public static void main(String[] args){
 
 	
-	/*
-	//call splash object
-	Splash splash = new Splash();
+	//declare to start getting user input
 	
-		//call splash screen	
-		splash.splashScreen();
-		
-		splash.the_endScreen();
-		}
+	Scanner scanner = new Scanner(System.in);
 	
-
-	*/
-
-	Person casey = new Person();
-
-	casey.setName("Casey");
-
-	System.out.println("Name: " + casey.getName());
-
-	Person mark = new Person("Mark", "cool guy", 45, "75");
-	mark.setHealth("33");
-	System.out.println("Name: " + casey.getName() + " " + casey.getAge() + " " + mark.getHealth());
-
-	//declare all objects at least one of each
-
-	Student student = new Student ();
-	Person fred = new  Person("Fred", "student", 19, "100");
-
-	//declare array of type parent class
-	Person[] person = new Person[3];
-
-	// put each object into array
-
-	person[0] = student;
-	person[1] = casey;
-	person[2] = mark;
-
-	//print out each object in array
-
-	System.out.println(person[0]);
-	System.out.println(person[1]);
-	System.out.println(person[2]);
-
-
-
-
-	//Student student_casey = new Student("Casey", "Student", 19, "100");
+	//Prompt the user with a question to provide input
+	
+	System.out.println("Welcome");
+	
+	System.out.println("What's your name?");
+	
+	String name = scanner.next();
+	
+	System.out.println("Hi " + name + "!");
+	
+	System.out.println("How many friends do you want?");
+	
+	int numberOfFriends = scanner.nextInt();
+	
+	//declare an array list 
+	ArrayList<Student> list = new ArrayList<>();
 	
 	
+	//initialize a list of students from user input
 	
+	for(int i = 0; i < numberOfFriends; i++){
+		list.add(new Student());
+		list.get(i).getGender("female");
+		list.get(i).setId(i);
+	}
 	
 	
 
