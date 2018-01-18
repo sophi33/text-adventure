a931b08338394876c1d7e6df2d0479851568cd24
Johns <csen@newroads.org>
2018-01-18 09:47:02 -0800

    developed random function, added isAlien to teacher

--- GamePlay.java
+++ GamePlay.java
@@ -1,5 +1,17 @@
 class GamePlay{
 	
+	public void init(){
 
+
+		//initialize a random amount of teachers for the student
+
+	}
+
+	public int getRandomNumber(int rangeMin, int rangeMax){
+
+		int randomNum = (int )(Math.random() * rangeMax + rangeMin);
+
+		return randomNum;
+	}
 
 }
