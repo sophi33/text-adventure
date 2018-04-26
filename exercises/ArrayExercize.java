b1285e29503b952edcc8017aeb76c3590c1ffad6
sophi33 <schertock@newroads.org>
2018-04-26 09:22:49 -0700

    made 5x5 array with random numbers

---
+++ exercises/ArrayExercize.java
@@ -0,0 +1,20 @@
+class MultidimensionalArray {
+   public static void main(String[] args) {
+
+   int [][] arr = new int[5][5];
+
+
+ for (int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+			int rand = (int)(Math.random() * 100);
+			arr[i][j] = rand;
+        }
+	}
+
+ for (int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+			System.out.println(arr[i][j]);
+        }
+	}
+}
+}
